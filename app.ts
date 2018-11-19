cb8182be95cb466b0c84d5c98cd4c50edf6ade7f
Ranjithprabhu Kumar <ranjithprabhu.k@gmail.com>
2018-11-19 16:52:44 +0530

    Mongo Db connection established

--- app.ts
+++ app.ts
@@ -1,27 +1,33 @@
 import * as express from 'express';
 import * as bodyParser from 'body-parser';
+import * as mongoose from 'mongoose';
 import { UserRoutes } from './routes/userRoutes';
 
 class App {
-
     public app: express.Application;
     public userRoutes: UserRoutes = new UserRoutes();
+    public mongoUrl: string = 'mongodb://localhost:27017/Users';
 
     constructor() {
         this.app = express();
-        this.config();
+        this.configureApp();
+        this.initializeMongoConnection();
 
         // Wire the Route to the app
         this.userRoutes.routes(this.app);
     }
 
-    private config(): void {
+    private configureApp(): void {
         // support application/json type post data
         this.app.use(bodyParser.json());
         // support application/x-www-form-urlencoded post data
         this.app.use(bodyParser.urlencoded({ extended: false }));
     }
 
+    private initializeMongoConnection(): void {
+        mongoose.Promise = global.Promise;
+        mongoose.connect(this.mongoUrl);
+    }
 }
 
 export default new App().app;
