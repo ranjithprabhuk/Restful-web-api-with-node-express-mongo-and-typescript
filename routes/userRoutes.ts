a80217aeedca5dbadaa5bbaca37da5bc57c3868e
Ranjithprabhu Kumar <ranjithprabhu.k@gmail.com>
2018-11-20 10:37:03 +0530

    get user and add user routes are added to the routes

--- routes/userRoutes.ts
+++ routes/userRoutes.ts
@@ -1,6 +1,9 @@
 import { Request, Response } from 'express';
+import { UserController } from '../controllers/userController';
 
 export class UserRoutes {
+    private userController: UserController = new UserController();
+
     public routes(app: any): void {
         app.route('/')
             .get((req: Request, res: Response) => {
@@ -8,5 +11,8 @@ export class UserRoutes {
                     message: 'Details fetched successfully',
                 });
             });
+
+        app.route('/users').get(this.userController.getUsers);
+        app.route('/users').post(this.userController.addNewUser);
     }
 }
